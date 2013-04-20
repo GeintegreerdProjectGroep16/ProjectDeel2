51742881067f4e8d3adc0b53b87a0c3c44731ca8
Eusebius <Eusebius.VanDoninck@student.phl.be>
2013-04-20 22:32:29 +0200

    Hibernate zou moeten werken, gewoon de netbeans tutorial aant volgen :)

--- src/java/Hibernate/festivalHelper.java
+++ src/java/Hibernate/festivalHelper.java
@@ -4,6 +4,8 @@
  */
 package Hibernate;
 
+import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -16,6 +18,19 @@ public class festivalHelper {
     
     public festivalHelper(){
         this.session = hibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    
+    public List getFestivalNames(int startID, int endID){
+        List<Festivals> festivalList = null;
+        try{
+            org.hibernate.Transaction tx = session.beginTransaction();
+            Query q = session.createQuery ("from festivals as festival where festival.fest_Id between '"+startID+"' and '"+endID+"'");
+            festivalList = (List<Festivals>) q.list();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return filmList;
+        }
     }
     
 }
